57133db5ea94365d6ad8d1969a5a130467d468d7
Brian Marete <bmarete10@gmail.com>
2025-02-12 11:13:29 +0300

    Add video background to hero element

--- src/app/ui/components/Hero.tsx
+++ src/app/ui/components/Hero.tsx
@@ -13,13 +13,25 @@ const Hero = () => {
   return (
     <section className="relative h-[90vh] -mt-10">
       <div className="image-container h-full -z-10 w-full">
-        <div className="bg-hero-image bg-no-repeat bg-cover h-full w-full"></div>
+        {/* <div className="bg-hero-image bg-no-repeat bg-cover h-full w-full"></div> */}
+        <video
+          className="absolute w-full h-full object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source
+            src="https://pub-94dc47df391c4556bac5370df0514d7d.r2.dev/bg-video.webm"
+            type="video/webm"
+          />
+        </video>
       </div>
       <motion.div
         initial={{ opacity: 0, y: 15 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.25, ease: "easeInOut" }}
-        className="absolute inset-0 flex flex-col justify-center items-center text-white "
+        className="absolute inset-0 flex flex-col justify-center items-center text-white z-10"
       >
         <span className="bg-mustard-yellow text-gray-900 px-4 py-2 mb-4 text-sm font-semibold">
           WE ARE JAWKIM CONSULTING ARCHITECTS LLP
@@ -30,7 +42,7 @@ const Hero = () => {
           WE BUILD YOUR DREAM
         </h1>
         <AnimatedText
-          className="text-xl font-thin mb-8 text-center"
+          className="text-xl font-thin mb-8 mx-8 text-center"
           text={text}
         />
         <AnchorLink
